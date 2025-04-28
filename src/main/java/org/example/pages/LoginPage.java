fea1bef11803f0543a988ef0a944f0047554674f
Suvarna-06 <sandhyasankesh@gmail.com>
2025-04-28 22:32:43 +0530

    28th April 2025

--- src/main/java/org/example/pages/LoginPage.java
+++ src/main/java/org/example/pages/LoginPage.java
@@ -2,6 +2,7 @@ package org.example.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage {
 
@@ -12,20 +13,51 @@ public class LoginPage {
     //By Locators
     private By emailInputLocator =By.name("email");
     private By passwordInputLocator = By.xpath("//input[@type='password']");
+    private By loginButtonLocator = By.xpath("//button[@type='submit']");
+    private By forgottenPasswordLinkLocator = By.linkText("Forgotten Password");
+    private By logoutLinkLocator = By.linkText("Logout");
 
+    // Constructor
+    public LoginPage(WebDriver driver){
+        this.driver = driver;
+    }
 
+    // APge Actions or Methods
+    public void enterEmail(String email){
+        WebElement emailInput = driver.findElement(emailInputLocator);
+        emailInput.sendKeys(email);
+    }
 
+    public void enterPassword(String password){
+        WebElement passwordInput = driver.findElement(passwordInputLocator);
+        passwordInput.sendKeys(password);
+    }
 
+    public void clickLoginButton(){
+        WebElement loginButton = driver.findElement(loginButtonLocator);
+        loginButton.click();
+    }
+    public void clickForgottenPasswordLink(){
+        WebElement forgottenPasswordLink = driver.findElement(forgottenPasswordLinkLocator);
+        forgottenPasswordLink.click();
+    }
 
+    public boolean checkForgotPwdLink(){
+        return driver.findElement(forgottenPasswordLinkLocator).isDisplayed();
+    }
+    public boolean checkLogutLink(){
+        return driver.findElement(logoutLinkLocator).isDisplayed();
+    }
 
+    public void login(String email, String password){
+        enterEmail(email);
+        enterPassword(password);
+        clickLoginButton();
+    }
 
-
-
-
-
-
-
-
-
+    public String getForgotPwdPageURl(){
+        String forgotPwdPageUrl = driver.getCurrentUrl();
+        return forgotPwdPageUrl;
+    }
 
 }
--- src/test/java/org/example/stepdefination/LoginPageStepDef.java
+++ src/test/java/org/example/stepdefination/LoginPageStepDef.java
@@ -1,4 +1,42 @@
 package org.example.stepdefination;
 
+import org.example.pages.LoginPage;
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import io.cucumber.java.en.Given;
+
+
 public class LoginPageStepDef {
+    private WebDriver driver;
+    private LoginPage loginPage;
+
+    @Before
+    public void setup(){
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown(){
+        if(driver!=null){
+            driver.quit();
+        }
+    }
+
+    @Given("I am on the OpenCart login page")
+    public void i_am_on_the_open_cart_login_page(){
+        driver.get("https://demo.opencart.com/en-gb?route=account/login");
+        loginPage = new LoginPage(driver);
+    }
+
+
+
+
+
+
+
+
+
 }
